242bf54a4ed068ea35fe933a597da0174a5102ea
Puneet Bansal <danny.explorer@gmail.com>
2014-04-10 20:55:25 -0500

    Added design patterns implementation

---
+++ abstractfactory/PepperoniPizza.java
@@ -0,0 +1,20 @@
+package abstractfactory;
+
+public class PepperoniPizza extends Pizza {
+	
+	PizzaIngredientFactory ingredientFactory;
+ 
+	public PepperoniPizza(PizzaIngredientFactory ingredientFactory) {
+		this.ingredientFactory = ingredientFactory;
+		// TODO - add required code
+	}
+ 
+	void prepare() {
+		System.out.println("Preparing " + name);
+		dough = ingredientFactory.createDough();
+		sauce = ingredientFactory.createSauce();
+		cheese = ingredientFactory.createCheese();
+		pepperoni = ingredientFactory.createPepperoni();
+		// TODO - implement method
+	}
+}
