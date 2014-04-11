e6b619c5b67c53139ad5e102313a6c118148b3b8
Puneet Bansal <danny.explorer@gmail.com>
2014-04-10 21:41:01 -0500

    Updated patterns

---
+++ decorator/src/CaseDecorator.java
@@ -0,0 +1,18 @@
+public class CaseDecorator {
+	
+	private MyStringReader myStringReader;
+	
+	public CaseDecorator(MyStringReader myStringReader) {
+		this.myStringReader = myStringReader;
+	}
+
+	public String getLowerCase() {
+		return this.myStringReader.readString().toLowerCase();
+	}
+	
+	public static void main(String[] args) {
+		MyStringReader caseConverter = new MyStringReader();
+		String str = new CaseDecorator(caseConverter).getLowerCase();
+		System.out.println(str);
+	}
+}
---
+++ decorator/src/MyStringReader.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class MyStringReader {
+	
+	private String str = "no string";
+
+	public String readString() {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			str = in.readLine();
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return str;
+	}
+}
